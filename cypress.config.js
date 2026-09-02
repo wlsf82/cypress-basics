fa32de96d4484d98ede1bc4a99aef2fd1b2767a1
Walmyr <wlsf82@gmail.com>
2026-09-02 23:39:57 +0200

    Delete removed config and define default browser

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  defaultBrowser: 'chrome',
 
   e2e: {
     setupNodeEvents(on, config) {
